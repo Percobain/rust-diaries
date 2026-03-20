ccc875f19644e706189a4cdf70f88a7366855ccb
Shreyans T <shreyans.tatiya@gmail.com>
2026-03-20 17:21:53 +0530

    init

---
+++ 002-hello_cargo/target/debug/deps/hello_cargo-7542a20e6c35f670.d
@@ -0,0 +1,5 @@
+/mnt/d/E/repos/rust-diaries/hello_cargo/target/debug/deps/hello_cargo-7542a20e6c35f670.d: src/main.rs
+
+/mnt/d/E/repos/rust-diaries/hello_cargo/target/debug/deps/libhello_cargo-7542a20e6c35f670.rmeta: src/main.rs
+
+src/main.rs:
---
+++ 002-hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/d/E/repos/rust-diaries/hello_cargo/target/debug/hello_cargo: /mnt/d/E/repos/rust-diaries/hello_cargo/src/main.rs
